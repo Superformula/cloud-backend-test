8fb59b55f3be8ddd11b80018092ca830363a547c
Bruno Rodrigues <brunin2509@gmail.com>
2021-04-29 01:27:22 -0300

    Added initial repo setup

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-inferrable-types': ['off'],
+    semi: ['error'],
+  },
+  settings: {},
+};
