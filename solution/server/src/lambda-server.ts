966c6cbc6beb3e0b6215cc2344aae60938b1de26
DESKTOP-E8UTAP9\Fabio <fabio.cmarques@hotmail.com>
2021-05-03 16:08:47 -0300

    Allow a local instance of DynamoDB when specified on environment

--- solution/server/src/lambda-server.ts
+++ solution/server/src/lambda-server.ts
@@ -4,14 +4,24 @@ import AWS from 'aws-sdk'
 import Geocoding, { GeocodeService } from '@mapbox/mapbox-sdk/services/geocoding'
 import resolvers from './resolvers/merge-resolvers'
 import typeDefs from './schemas/merge-schemas'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
 dotenv.config()
 
 export type CloudBackEndTestContext = {
-	dynamo: AWS.DynamoDB.DocumentClient
+	dynamo: DocumentClient
 	mapbox: GeocodeService
 }
-const dynamo = new AWS.DynamoDB.DocumentClient()
+
+if (process.env.LOCAL_DYNAMODB) {
+	AWS.config.update({
+		dynamodb: {
+			endpoint: process.env.LOCAL_DYNAMODB,
+		},
+	})
+}
+
+const dynamo: DocumentClient = new AWS.DynamoDB.DocumentClient()
 
 // Mapbox API key on environment
 const mapboxService = Geocoding({
