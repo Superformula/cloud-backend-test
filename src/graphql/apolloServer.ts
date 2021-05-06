13271385baf8b9ebbe39924bef5b160fe3d88ba4
mariogruizdiaz <mariogrd@gmail.com>
2021-05-06 11:03:16 -0300

    folder structure for resolvers and types

--- src/graphql/apolloServer.ts
+++ src/graphql/apolloServer.ts
@@ -1,22 +1,16 @@
-import { ApolloServer, IResolvers, gql } from 'apollo-server-lambda';
+import { ApolloServer, IResolvers } from 'apollo-server-lambda';
+import * as queries from './resolvers/queries';
+import * as mutations from './resolvers/mutations';
+import typeDefs from './type-defs';
 
 const NODE_ENV = process.env.NODE_ENV;
 
 const IS_DEV = !NODE_ENV || !['production'].includes(NODE_ENV);
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
 const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
+  Mutation: mutations,
+  Query: queries,
+} as IResolvers;
 
 const apolloServer = new ApolloServer({
   typeDefs,
