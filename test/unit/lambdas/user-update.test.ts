799bbd1c65ba0a40bd35f431b9c1ed8de98bc3f0
Carlos Torres <carlos@liberet.com>
2022-01-05 16:59:35 -0600

    feat: add more unit tests on update user lambda

--- test/unit/lambdas/user-update.test.ts
+++ test/unit/lambdas/user-update.test.ts
@@ -1,3 +1,6 @@
+import * as AWS from 'aws-sdk'
+import * as AWSMock from 'aws-sdk-mock'
+import { UpdateItemInput } from 'aws-sdk/clients/dynamodb'
 import { handler } from '../../../src/lambdas/user-update'
 
 describe('Should return error object', () => {
@@ -11,5 +14,41 @@ describe('Should return error object', () => {
     let response = await handler({ arguments: { user: { id: '123' } } })
     let expectedError = { error: { message: 'No properties to update provided', type: 'ValidationError' } }
     expect(response).toMatchObject(expectedError)
+  })
+
+  test('On DynamoDB error', async () => {
+    let expectedError = {
+      error: {
+        message: 'Error: Execution update, caused a Dynamodb error, please take a look at your CloudWatch Logs.',
+        type: 'ConfigError',
+      },
+    }
+
+    let response = await handler({ arguments: { user: { id: '123', address: '123 Fake St' } } })
+    expect(response).toStrictEqual(expectedError)
+  })
+})
+
+describe('Update user', () => {
+  test('Update user and return all attributes', async () => {
+    let updatedUser = {
+      id: '123',
+      name: 'Bruce Wayne',
+      description: 'CEO of Wayne Enterprises',
+      address: '123 Gotham St, Gotham',
+      imageUrl: 'gotham.com/bruce-wayne.png',
+      dob: '1969-08-07',
+    }
+
+    AWSMock.setSDKInstance(AWS)
+    AWSMock.mock('DynamoDB.DocumentClient', 'update', (params: UpdateItemInput, callback: Function) => {
+      callback(null, updatedUser)
+    })
+
+    const input: UpdateItemInput = { TableName: '', Key: {} }
+    const client = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
+    expect(await client.update(input).promise()).toStrictEqual(updatedUser)
+
+    AWSMock.restore('DynamoDB.DocumentClient')
   })
 })
