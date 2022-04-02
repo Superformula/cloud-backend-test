d454e334c8fab04185a7e337f663ab1629a1441f
RodrigoAGM <rod-gm@hotmail.com>
2022-04-02 16:09:06 -0500

    chore: use .graphql schema and config type auto-generation

--- server/src/lambda.ts
+++ server/src/lambda.ts
@@ -1,6 +1,9 @@
 import { ApolloServer } from 'apollo-server-lambda';
+import { readFileSync } from 'fs';
 import { resolvers } from './graphql/resolvers/resolvers';
-import { typeDefs } from './graphql/schema/schema';
+
+// Read types from .graphql schema
+const typeDefs = readFileSync('./graphql/schema/schema.graphql', 'utf8');
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
