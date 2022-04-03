43a0b12d104b287fb106d38909ed21ccc59b370b
RodrigoAGM <rod-gm@hotmail.com>
2022-04-02 19:07:02 -0500

    feat: user create mutation implemented

--- server/src/graphql/resolvers/mutation.ts
+++ server/src/graphql/resolvers/mutation.ts
@@ -2,5 +2,8 @@ import { AppContext } from '../../types/types';
 import { MutationResolvers } from '../types/types';
 
 export const Mutation: MutationResolvers<AppContext> = {
-
+  createUser: async (_parent, args, context) => {
+    const user = await context.userService.createUser(args.data);
+    return user;
+  },
 };
--- server/src/service/user.service.ts
+++ server/src/service/user.service.ts
@@ -1,4 +1,8 @@
+import { ApolloError } from 'apollo-server-lambda';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { v4 as uuid } from 'uuid';
+import { User, UserInput } from '../graphql/types/types';
+import { dateIsValid, formatDateOnly, getCurrentDateStr } from '../utils/date.util';
 
 export class UserService {
   private database: DocumentClient;
@@ -8,5 +12,33 @@ export class UserService {
   constructor(db: DocumentClient, tableName: string) {
     this.database = db;
     this.tableName = tableName;
+  }
+
+  async createUser(data: UserInput): Promise<User> {
+    try {
+      // Validate provided date
+      if (!dateIsValid(data.dob)) {
+        return Promise.reject(new ApolloError('The provided date of birth is invalid.'));
+      }
+
+      const user: User = {
+        id: uuid(),
+        createdAt: getCurrentDateStr(),
+        description: data.description,
+        dob: formatDateOnly(data.dob),
+        name: data.name,
+        imageUrl: data.imageUrl,
+        address: data.address,
+      };
+
+      await this.database.put({
+        TableName: this.tableName,
+        Item: user,
+      }).promise();
+
+      return Promise.resolve(user);
+    } catch (error) {
+      return Promise.reject(new ApolloError('Error while creating user'));
+    }
   }
 }
