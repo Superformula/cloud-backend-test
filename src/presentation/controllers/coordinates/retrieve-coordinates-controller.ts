0cd658288bba52760971ca72306862f3705c5380
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 16:45:46 -0400

    feat: ensure RetrieveCoordinatesController calls RetrieveCoordinates with correct address

--- src/presentation/controllers/coordinates/retrieve-coordinates-controller.ts
+++ src/presentation/controllers/coordinates/retrieve-coordinates-controller.ts
@@ -1,7 +1,12 @@
+import { RetrieveCoordinates } from '@domain/usecases'
 import { Controller, HttpResponse } from '@presentation/protocols'
 
 export class RetrieveCoordinatesController implements Controller {
+  constructor (private readonly retrieveCoordinates: RetrieveCoordinates) {}
+
   async handle (request: any): Promise<HttpResponse> {
+    const { address } = request.body
+    this.retrieveCoordinates.retrieveCoordinates(address)
     return null
   }
 }
