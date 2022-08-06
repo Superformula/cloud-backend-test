910d2190eee596c5d6508ea8e5a1aad7a6ad4ea8
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 15:42:10 -0400

    test: ensure DbRetrieveCoordinates throws BadRequestError if RetrieveCoordinatesRepository returns null

--- src/data/usecases/db-retrieve-coordinates.spec.ts
+++ src/data/usecases/db-retrieve-coordinates.spec.ts
@@ -1,4 +1,5 @@
 import { RetrieveCoordinatesRepositorySpy } from '@data/test'
+import { BadRequestError } from '@domain/errors/http-errors'
 import { faker } from '@faker-js/faker'
 import { DbRetrieveCoordinates } from './db-retrieve-coordinates'
 
@@ -32,6 +33,13 @@ describe('DbRetrieveCoordinates', () => {
     const { retrieveCoordinatesRepositorySpy, sut } = makeSut()
     const coordinates = await sut.retrieveCoordinates(faker.address.city())
     expect(coordinates).toEqual(retrieveCoordinatesRepositorySpy.result)
+  })
+
+  test('Should throw BadRequestError if RetrieveCoordinatesRepository returns null', async () => {
+    const { retrieveCoordinatesRepositorySpy, sut } = makeSut()
+    jest.spyOn(retrieveCoordinatesRepositorySpy, 'retrieveCoordinates').mockReturnValueOnce(null)
+    const promise = sut.retrieveCoordinates(faker.address.city())
+    await expect(promise).rejects.toThrow(new BadRequestError('No information found for the given address.'))
   })
 
   test('Should throw if RetrieveCoordinatesRepository throws', async () => {
