f52f16cf64a8edc9d74ff942efc8163770721d89
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 14:07:13 -0400

    feat: ensure MapBoxHttpClientAdapter returns latitude and longitude if feature list is not empty

--- src/infrastructure/http-clients/maps/map-box/mapbox-http-client-adapter.ts
+++ src/infrastructure/http-clients/maps/map-box/mapbox-http-client-adapter.ts
@@ -1,5 +1,6 @@
 import { RetrieveCoordinatesHttpClient } from '@data/protocols/http-clients/maps'
 import { httpMapClientsConstants } from '@infrastructure/http-clients/settings'
+import { IForwardGeoCodingResponse } from './interfaces/Imap-box-http-client'
 
 export class MapBoxHttpClientAdapter implements RetrieveCoordinatesHttpClient {
   async retrieveCoordinates (address: string): Promise<RetrieveCoordinatesHttpClient.Result> {
@@ -12,11 +13,19 @@ export class MapBoxHttpClientAdapter implements RetrieveCoordinatesHttpClient {
 
     const mapboxConfig = httpMapClientsConstants.mapbox
 
-    await fetch(
+    const response: Response = await fetch(
       `${mapboxConfig.GEOCODING_BASE_URL}${mapboxConfig.GEOCODING_ENDPOINT_PLACES}${address}.json?access_token=${mapboxConfig.ACCESS_TOKEN}`,
       options
     )
 
-    return null
+    const data: IForwardGeoCodingResponse = await response.json()
+
+    if (data.features.length) {
+      const [latitude, longitude] = data.features[0].center
+      return {
+        latitude,
+        longitude
+      }
+    }
   }
 }
