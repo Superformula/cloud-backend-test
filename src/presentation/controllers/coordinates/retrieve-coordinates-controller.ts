33bae1ea74cd922bc48595ff9d2149d9e6f3e500
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 17:13:24 -0400

    feat: ensure RetrieveCoordinatesController returns catchError if RetrieveCoordinates throws

--- src/presentation/controllers/coordinates/retrieve-coordinates-controller.ts
+++ src/presentation/controllers/coordinates/retrieve-coordinates-controller.ts
@@ -1,14 +1,18 @@
 import { RetrieveCoordinates } from '@domain/usecases'
-import { ok } from '@presentation/helpers'
+import { catchError, ok } from '@presentation/helpers'
 import { Controller, HttpResponse } from '@presentation/protocols'
 
 export class RetrieveCoordinatesController implements Controller {
   constructor (private readonly retrieveCoordinates: RetrieveCoordinates) {}
 
   async handle (request: any): Promise<HttpResponse> {
-    const { address } = request.body
+    try {
+      const { address } = request.body
 
-    const coordinates = await this.retrieveCoordinates.retrieveCoordinates(address)
-    return ok(coordinates)
+      const coordinates = await this.retrieveCoordinates.retrieveCoordinates(address)
+      return ok(coordinates)
+    } catch (error) {
+      return catchError(error)
+    }
   }
 }
