53199ba83ac356c9a7a2a2d5699132e66ee6daa7
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 10:04:30 -0400

    chore: update jest config to support module name mapping

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '@data/(.*)$': '<rootDir>/src/data/$1'
+  },
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
