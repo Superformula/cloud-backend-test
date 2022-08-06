ff3f49c28b10d430b507de0367b6ce6a707b55f8
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 13:47:13 -0400

    feat: ensure MapBoxHttpClientAdapter calls fetch method with correct values

--- src/infrastructure/http-clients/maps/map-box/mapbox-http-client-adapter.ts
+++ src/infrastructure/http-clients/maps/map-box/mapbox-http-client-adapter.ts
@@ -3,6 +3,20 @@ import { httpMapClientsConstants } from '@infrastructure/http-clients/settings'
 
 export class MapBoxHttpClientAdapter implements RetrieveCoordinatesHttpClient {
   async retrieveCoordinates (address: string): Promise<RetrieveCoordinatesHttpClient.Result> {
+    const options: RequestInit = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const mapboxConfig = httpMapClientsConstants.mapbox
+
+    await fetch(
+      `${mapboxConfig.GEOCODING_BASE_URL}${mapboxConfig.GEOCODING_ENDPOINT_PLACES}${address}.json?access_token=${mapboxConfig.ACCESS_TOKEN}`,
+      options
+    )
+
     return null
   }
 }
