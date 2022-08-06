be6fa7ccc1176ba22a43ba499c329e7dc3a91e2f
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 16:15:04 -0400

    feat: add RetrieveCoordinateController production class

---
+++ src/presentation/controllers/coordinates/retrieve-coordinates-controller.ts
@@ -0,0 +1,7 @@
+import { Controller, HttpResponse } from '@presentation/protocols'
+
+export class RetrieveCoordinatesController implements Controller {
+  async handle (request: any): Promise<HttpResponse> {
+    return null
+  }
+}
