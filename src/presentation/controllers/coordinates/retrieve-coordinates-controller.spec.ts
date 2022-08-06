9f30b1286999f3464357c99ab723eba4529bd123
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 16:49:42 -0400

    refactor: move sut creation to its own factory method

--- src/presentation/controllers/coordinates/retrieve-coordinates-controller.spec.ts
+++ src/presentation/controllers/coordinates/retrieve-coordinates-controller.spec.ts
@@ -1,10 +1,24 @@
 import { mockRetrieveCoordinatesRequest, RetrieveCoordinatesSpy } from '@presentation/test'
 import { RetrieveCoordinatesController } from './retrieve-coordinates-controller'
 
+interface SutTypes {
+  sut: RetrieveCoordinatesController
+  retrieveCoordinatesSpy: RetrieveCoordinatesSpy
+}
+
+const makeSut = (): SutTypes => {
+  const retrieveCoordinatesSpy = new RetrieveCoordinatesSpy()
+  const sut = new RetrieveCoordinatesController(retrieveCoordinatesSpy)
+
+  return {
+    retrieveCoordinatesSpy,
+    sut
+  }
+}
+
 describe('RetrieveCoordinatesController', () => {
   test('Should call RetrieveCoordinates with correct address', async () => {
-    const retrieveCoordinatesSpy = new RetrieveCoordinatesSpy()
-    const sut = new RetrieveCoordinatesController(retrieveCoordinatesSpy)
+    const { retrieveCoordinatesSpy, sut } = makeSut()
     await sut.handle(mockRetrieveCoordinatesRequest)
     expect(retrieveCoordinatesSpy.address).toBe(mockRetrieveCoordinatesRequest.body.address)
   })
