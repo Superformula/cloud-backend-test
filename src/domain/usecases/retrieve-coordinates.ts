aa44dd25a4e3ae582c5b6078cdfd61e212a5b61a
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 09:45:18 -0400

    refactor: add namespace to RetrieveCoodinates usecase to constraint domain layer dependency

--- src/domain/usecases/retrieve-coordinates.ts
+++ src/domain/usecases/retrieve-coordinates.ts
@@ -1,5 +1,9 @@
 import { Coordinate } from '../models'
 
 export interface RetrieveCoordinates {
-  retrieveCoordinates: (address: string) => Promise<Coordinate>
+  retrieveCoordinates: (address: string) => Promise<RetrieveCoordinates.Result>
+}
+
+export namespace RetrieveCoordinates {
+  export type Result = Coordinate
 }
