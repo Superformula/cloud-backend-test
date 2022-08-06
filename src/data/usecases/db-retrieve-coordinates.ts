b6e6ef6d646e0c7407bf4ccadb096329dc0a0ae7
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-06 15:41:51 -0400

    feat: ensure DbRetrieveCoordinates throws BadRequestError if RetrieveCoordinatesRepository returns null

--- src/data/usecases/db-retrieve-coordinates.ts
+++ src/data/usecases/db-retrieve-coordinates.ts
@@ -1,10 +1,16 @@
 import { RetrieveCoordinatesHttpClient } from '@data/protocols/http-clients/maps'
+import { BadRequestError } from '@domain/errors/http-errors'
 import { RetrieveCoordinates } from '@domain/usecases'
 
 export class DbRetrieveCoordinates implements RetrieveCoordinates {
   constructor (private readonly retrieveCoordinatesRepository: RetrieveCoordinatesHttpClient) {}
 
   async retrieveCoordinates (address: string): Promise<RetrieveCoordinates.Result> {
-    return await this.retrieveCoordinatesRepository.retrieveCoordinates(address)
+    const coordinates = await this.retrieveCoordinatesRepository.retrieveCoordinates(address)
+    if (!coordinates) {
+      throw new BadRequestError('No information found for the given address.')
+    }
+
+    return coordinates
   }
 }
