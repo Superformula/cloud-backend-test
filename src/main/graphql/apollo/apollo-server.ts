e32db0fc46e522ea1b3f3c1b0586e4fa75f7d291
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-07 15:34:22 -0400

    feat: add CoordinateResolver to buildSchema

--- src/main/graphql/apollo/apollo-server.ts
+++ src/main/graphql/apollo/apollo-server.ts
@@ -1,10 +1,11 @@
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
+import { CoordinateResolver } from '../resolvers'
 
 export const setupApolloServer = async (): Promise<ApolloServer> =>
   new ApolloServer({
     schema: await buildSchema({
-      resolvers: [null]
+      resolvers: [CoordinateResolver]
     }),
     context: ({ req, res }) => ({ req, res })
   })
