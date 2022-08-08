0f4ee4ec591433c9d18e200575d72d34a011fd7f
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-08 13:05:55 -0400

    test: ensure CoordinateResolver returns UserInputError on invalid input

--- src/main/graphql/resolvers/coordinate-resolver.spec.ts
+++ src/main/graphql/resolvers/coordinate-resolver.spec.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata'
 import { setupApp } from '@main/config/app'
-import request from 'supertest'
 import { Express } from 'express'
+import request from 'supertest'
 
 let app: Express
+let query: string
 
 describe('CoordinateResolver', () => {
   beforeAll(async () => {
@@ -11,12 +12,14 @@ describe('CoordinateResolver', () => {
   })
 
   describe('coordinate query', () => {
-    const query = `query {
-      coordinate(address: "barranco") {
-        latitude
-        longitude
-      }
-    }`
+    beforeEach(async () => {
+      query = `query {
+        coordinate(address: "barranco") {
+          latitude
+          longitude
+        }
+      }`
+    })
 
     test('Should return coordinates on success', async () => {
       const res = await request(app)
@@ -26,6 +29,23 @@ describe('CoordinateResolver', () => {
       expect(res.status).toBe(200)
       expect(res.body.data.coordinate.latitude).toBe(-12.144197)
       expect(res.body.data.coordinate.longitude).toBe(-77.020068)
+    })
+
+    test('Should return UserInputError on invalid input', async () => {
+      query = `query {
+        coordinate(address: "barrancosss") {
+          latitude
+          longitude
+        }
+      }`
+
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+
+      expect(res.status).toBe(400)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('No information found for the given address.')
     })
   })
 })
