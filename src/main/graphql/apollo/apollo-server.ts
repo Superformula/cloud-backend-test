67738902702fc0fe6e74ccb4a772b081b1ed08b3
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-08 11:43:25 -0400

    feat: add status code on thrown exepcetions

--- src/main/graphql/apollo/apollo-server.ts
+++ src/main/graphql/apollo/apollo-server.ts
@@ -1,11 +1,34 @@
 import { ApolloServer } from 'apollo-server-express'
+import { GraphQLError } from 'graphql'
 import { buildSchema } from 'type-graphql'
 import { CoordinateResolver } from '../resolvers'
+
+const checkErrorExists = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
+}
+
+const setHttpPlugin = {
+  async requestDidStart () {
+    return {
+      async willSendResponse ({ response, errors }) {
+        errors?.forEach((error: GraphQLError) => {
+          response.data = undefined
+          if (checkErrorExists(error, 'UserInputError')) {
+            response.http.status = 400
+          } else {
+            response.http.status = 500
+          }
+        })
+      }
+    }
+  }
+}
 
 export const setupApolloServer = async (): Promise<ApolloServer> =>
   new ApolloServer({
     schema: await buildSchema({
       resolvers: [CoordinateResolver]
     }),
-    context: ({ req, res }) => ({ req, res })
+    context: ({ req, res }) => ({ req, res }),
+    plugins: [setHttpPlugin as any]
   })
