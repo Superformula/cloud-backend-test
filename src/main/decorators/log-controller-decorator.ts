ce1d2e324275e9de89ff7c69931378198896c38d
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-08 19:53:28 -0400

    refactor: comment unused if statement

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -5,14 +5,14 @@ export class LogControllerDecorator implements Controller {
 
   async handle<T>(httpRequest: T): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
-    if (httpResponse.statusCode === 500 && process.env.NODE_ENV === 'production') {
-      // const log: Log = createLog(httpRequest, httpResponse)
+    // if (httpResponse.statusCode === 500 && process.env.NODE_ENV === 'production') {
+    // const log: Log = createLog(httpRequest, httpResponse)
 
-      // here we can make use of a service to create a log in the database to save this error
-      // of course we only want to do this in production
+    // here we can make use of a service to create a log in the database to save critical errors
+    // of course we only want to do this in production
 
-      // await this.logErrorRepository.logError(log)
-    }
+    // await this.logErrorRepository.logError(log)
+    // }
     return httpResponse
   }
 }
