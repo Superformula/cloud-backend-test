cd29344365fdf382d77ac1641afea6d9a4c4b5bb
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-08 10:39:16 -0400

    test: ensure ApolloServerResolverAdapter adds agrs to request body

---
+++ src/main/adapters/apollo-server-resolver-adapter.spec.ts
@@ -0,0 +1,14 @@
+import { faker } from '@faker-js/faker'
+import { ControllerMock, ControllerSpy } from '@main/test'
+import { adaptResolver } from './apollo-server-resolver-adapter'
+
+describe('ApolloServerResolverAdapter', () => {
+  test('Should add agrs to request body', async () => {
+    const controllerMock = new ControllerMock()
+    const req = jest.fn() as any
+    req.body = {}
+    const args = { [faker.datatype.string()]: faker.random.word() }
+    await adaptResolver(controllerMock, req, args)
+    expect(req.body).toEqual(args)
+  })
+})
