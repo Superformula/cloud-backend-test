531c3cc58eb0a0ad4be04279f070393f24f3e8cf
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-08 09:14:43 -0400

    feat: ensure LogControllerDecorator returns httpResponse from controller

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -5,6 +5,14 @@ export class LogControllerDecorator implements Controller {
 
   async handle<T>(httpRequest: T): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
-    return null
+    if (httpResponse.statusCode === 500 && process.env.NODE_ENV === 'production') {
+      // const log: Log = createLog(httpRequest, httpResponse)
+
+      // here we can make use of a service to create a log in the database to save this error
+      // of course we only want to do this in production
+
+      // await this.logErrorRepository.logError(log)
+    }
+    return httpResponse
   }
 }
