29564d0903e5ca820984710333e9e98db256fcc1
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-08 10:50:17 -0400

    refactor: move adaptResolver creation to its own factory method

--- src/main/adapters/apollo-server-resolver-adapter.spec.ts
+++ src/main/adapters/apollo-server-resolver-adapter.spec.ts
@@ -2,29 +2,41 @@ import { faker } from '@faker-js/faker'
 import { ControllerMock, ControllerSpy } from '@main/test'
 import { adaptResolver } from './apollo-server-resolver-adapter'
 
+interface SutTypes {
+  req: any
+  controllerSpy: ControllerSpy
+  args: object
+  result: Promise<any>
+}
+
+const makeSut = async (): Promise<SutTypes> => {
+  const controllerSpy = new ControllerSpy()
+  const req = jest.fn() as any
+  req.body = {}
+  const args = { [faker.datatype.string()]: faker.random.word() }
+  const result = await adaptResolver(controllerSpy, req, args)
+
+  return {
+    req,
+    controllerSpy,
+    args,
+    result
+  }
+}
+
 describe('ApolloServerResolverAdapter', () => {
   test('Should add agrs to request body', async () => {
-    const controllerMock = new ControllerMock()
-    const req = jest.fn() as any
-    req.body = {}
-    const args = { [faker.datatype.string()]: faker.random.word() }
-    await adaptResolver(controllerMock, req, args)
+    const { req, args } = await makeSut()
     expect(req.body).toEqual(args)
   })
 
   test('Should call Controller with correct arguments', async () => {
-    const controllerSpy = new ControllerSpy()
-    const req = jest.fn() as any
-    req.body = {}
-    const args = { [faker.datatype.string()]: faker.random.word() }
-    await adaptResolver(controllerSpy, req, args)
+    const { req, controllerSpy } = await makeSut()
     expect(controllerSpy.request).toEqual(req)
   })
 
   test('Should return Controller\'s response', async () => {
-    const controllerSpy = new ControllerSpy()
-    const req = jest.fn() as any
-    const result = await adaptResolver(controllerSpy, req, null)
+    const { result, controllerSpy } = await makeSut()
     expect(result).toEqual(controllerSpy.httpResponse.body)
   })
 })
