95f17727bf6a799843da570fe01014f0e98ddb5a
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-08 18:35:24 -0400

    test: ensure ErrorMiddleware returns userError with default error message on INTERNAL_SERVER_ERROR

--- src/main/lib/error-middleware.spec.ts
+++ src/main/lib/error-middleware.spec.ts
@@ -2,7 +2,7 @@ import { mockGraphQLError } from '@main/test'
 import { ErrorMiddleware } from './error-middleware'
 
 describe('ErrorMiddleware', () => {
-  test('Should log error on production environemnt', async () => {
+  test('Should log error in production environment on INTERNAL_SERVER_ERROR', async () => {
     console.error = jest.fn()
     const graphQLError = mockGraphQLError()
     ErrorMiddleware.prototype.sendErrorProd(graphQLError)
@@ -11,6 +11,16 @@ describe('ErrorMiddleware', () => {
       code: 'INTERNAL_SERVER_ERROR',
       message: graphQLError.message,
       stack: graphQLError.originalError.stack
+    })
+  })
+
+  test('Should return userError with default error message in production environment on INTERNAL_SERVER_ERROR', async () => {
+    const graphQLError = mockGraphQLError()
+    const response = ErrorMiddleware.prototype.sendErrorProd(graphQLError)
+    expect(response).toEqual({
+      code: graphQLError.extensions.code,
+      path: graphQLError.path,
+      message: 'We are sorry, we have detected an error. Our team is working to solve it as soon as possible.'
     })
   })
 })
