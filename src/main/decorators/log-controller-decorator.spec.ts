652382daaa280d9894df245bdc1ce0fc68d9a4fc
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-08 09:00:44 -0400

    test: ensure LogControllerDecorator calls controller handle with correct arguments

---
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -0,0 +1,40 @@
+import { faker } from '@faker-js/faker'
+import { Controller, HttpResponse } from '@presentation/protocols'
+import { LogControllerDecorator } from './log-controller-decorator'
+
+class ControllerSpy implements Controller {
+  request: any
+  httpResponse: HttpResponse = {
+    body: faker.datatype.json(),
+    statusCode: faker.helpers.arrayElement([200, 201])
+  }
+
+  async handle (request: any): Promise<HttpResponse> {
+    this.request = request
+    return this.httpResponse
+  }
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerSpy: ControllerSpy
+}
+
+const makeSut = (): SutTypes => {
+  const controllerSpy = new ControllerSpy()
+  const sut = new LogControllerDecorator(controllerSpy)
+
+  return {
+    controllerSpy,
+    sut
+  }
+}
+
+describe('LogControllerDecorator', () => {
+  test('Should call controller handle with correct arguments', async () => {
+    const { controllerSpy, sut } = makeSut()
+    const request = faker.animal.bear()
+    await sut.handle(request)
+    expect(controllerSpy.request).toEqual(request)
+  })
+})
