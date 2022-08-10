ba8cd0f25ff0af71bc5f9e4757ea5e6d8d6ae3f2
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-09 20:35:59 -0400

    refactor: update controller input

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,13 +1,13 @@
 import { Controller } from '@presentation/protocols'
-import { ApolloError, UserInputError } from 'apollo-server-express'
-import { Request } from 'express'
+import { ApolloError, UserInputError } from 'apollo-server-lambda'
 
-export const adaptResolver = async (controller: Controller, req: Request, args: object): Promise<any> => {
-  for (const key in args) {
-    req.body[key] = args[key]
+export const adaptResolver = async (controller: Controller, args?: object, context?: any): Promise<any> => {
+  const request = {
+    ...(args || {})
+    // here we can add contextual information
   }
 
-  const httpResponse = await controller.handle(req)
+  const httpResponse = await controller.handle(request)
 
   switch (httpResponse.statusCode) {
     case 200: return httpResponse.body
