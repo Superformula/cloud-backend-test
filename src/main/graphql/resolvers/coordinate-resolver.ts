45296a490a04cd5716e62d63237c57b293e654fa
DiegoSalas27 <dominicsc2hs@gmail.com>
2022-08-09 20:39:51 -0400

    refactor: update coordinate resolver parameters

--- src/main/graphql/resolvers/coordinate-resolver.ts
+++ src/main/graphql/resolvers/coordinate-resolver.ts
@@ -1,13 +1,12 @@
 import { adaptResolver } from '@main/adapters'
 import { makeRetrieveCoordinatesController } from '@main/factories/controllers'
-import { Arg, Ctx, Query, Resolver } from 'type-graphql'
+import { Arg, Query, Resolver } from 'type-graphql'
 import { Coordinate } from '../entities'
-import { MyContext } from '../my-context'
 
 @Resolver()
 export class CoordinateResolver {
   @Query(() => Coordinate, { nullable: true })
-  async coordinate (@Arg('address', type => String) address: string, @Ctx() { req }: MyContext): Promise<Coordinate> {
-    return await adaptResolver(makeRetrieveCoordinatesController(), req, { address })
+  async coordinate (@Arg('address', type => String) address: string): Promise<Coordinate> {
+    return await adaptResolver(makeRetrieveCoordinatesController(), { address })
   }
 }
