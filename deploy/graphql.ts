2b7c7e2c4edd2f42af874fcadbf1a37f8bf497c4
himabindugit <bindumca7ster@gmail.com>
2022-08-12 17:25:35 -0400

    chore: add deploy supported scripts

---
+++ deploy/graphql.ts
@@ -0,0 +1,17 @@
+import * as cdk from '@aws-cdk/core';
+
+export enum Environment {
+  Production = 'prd',
+  Staging = 'stg',
+  QA = 'qa',
+  Development = 'dev',
+}
+
+export interface StackProps extends cdk.StackProps {
+  apiKey: string;
+  environment: Environment;
+  imageTag?: string;
+  hostedZone: string;
+  authority: string;
+  serviceName: string;
+}
