23d4a9287220cda8dc9b53443afa3364139264a6
himabindugit <bindumca7ster@gmail.com>
2022-08-12 17:30:37 -0400

    chore: tests utilities

---
+++ src/test/utilities/testData.ts
@@ -0,0 +1,7 @@
+export default Object.freeze({
+  ADDRESS: '29 Main St Watertown, MA 02472',
+  INCOMPLETE_ADDRESS: '79 Saco St',
+  UK_ADDRESS: '71 Cherry Court SOUTHAMPTON SO53 5PD UK',
+  INVALID_API_KEY: 'badKey',
+  BAD_ADDRESS: 'junk',
+});
