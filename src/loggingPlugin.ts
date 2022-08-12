0e5c213c7c2cac4f6ca8a708f8df953f10f88a40
himabindugit <bindumca7ster@gmail.com>
2022-08-12 16:32:49 -0400

    chore: logging plugin for logs for more info

--- src/loggingPlugin.ts
+++ src/loggingPlugin.ts
@@ -1,14 +1,18 @@
+import { GraphQLRequestContext } from 'apollo-server-core';
+
 export const loggingPlugin = {
   // Fires whenever a GraphQL request is received from a client.
   // eslint-disable-next-line require-await
-  async requestDidStart(requestContext: any) {
-    console.log('Request started! Query:\n' + requestContext.request.query);
+  async requestDidStart(requestContext: GraphQLRequestContext) {
+    console.log(
+      'Request started! Request Context Query:\n' + requestContext.request.query
+    );
 
     return {
       // Fires whenever Apollo Server will parse a GraphQL
       // request to create its associated document AST.
       // eslint-disable-next-line require-await
-      async parsingDidStart(requestContext: any) {
+      async parsingDidStart(requestContext: GraphQLRequestContext) {
         console.log(
           'Parsing started! variables:\n',
           requestContext.request.variables
@@ -18,7 +22,7 @@ export const loggingPlugin = {
       // Fires whenever Apollo Server will validate a
       // request's document AST against your GraphQL schema.
       // eslint-disable-next-line require-await
-      async validationDidStart(requestContext: any) {
+      async validationDidStart(requestContext: GraphQLRequestContext) {
         console.log('Validation started! context:\n', requestContext.context);
       },
     };
