cee1771178ea764144e74f0111339f03ac1fb5d2
himabindugit <bindumca7ster@gmail.com>
2022-08-12 17:29:54 -0400

    chore: implementation around env files and global configs

---
+++ src/conf/constants.ts
@@ -0,0 +1,8 @@
+export const ERROR_MESSAGES = Object.freeze({
+  INVALID_API_KEY: 'Please provide valid API Key for GeoCoding API!',
+  INVALID_ADDRESS: 'Please provide valid address!',
+  INCOMPLETE_ADDRESS:
+    'There are multiple addresses for the given address, Please provide full address!',
+});
+export const INVALID_API_KEY_ERROR =
+  'Error: Status is REQUEST_DENIED. The provided API key is invalid.';
